830d67eeefe63f9f840ba2e023fb03a1c0e968ef
sherif ghaffar <sherif-99@hotmail.com>
2016-03-12 17:52:17 -0500

    Worked on intents and listeners

--- app/src/main/java/com/example/cstuser/assignment2/MainActivity.java
+++ app/src/main/java/com/example/cstuser/assignment2/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.cstuser.assignment2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import android.os.Handler;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -13,7 +17,17 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainui);
 
+        Toast.makeText(getApplicationContext(), "Welcome", Toast.LENGTH_SHORT).show();
         // DISPLAY MESSAGE THAT IS DELAYED
-        // CREATE NEW INTENT
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent("com.example.cstuser.assignment2.PickInvestorAct");
+                startActivity(i);
+            }
+        }, 3000);
+
     }
 }
--- app/src/main/java/com/example/cstuser/assignment2/PickInvestorAct.java
+++ app/src/main/java/com/example/cstuser/assignment2/PickInvestorAct.java
@@ -1,7 +1,36 @@
 package com.example.cstuser.assignment2;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by sherif on 2016-03-12.
  */
-public class PickInvestorAct {
+public class PickInvestorAct extends Activity implements View.OnClickListener {
+    Button returnButton;
+    Button exitButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pickinvestorui);
+
+
+        returnButton = (Button) this.findViewById(R.id.returnButton);
+        exitButton = (Button) this.findViewById(R.id.exitButton);
+
+        returnButton.setOnClickListener(this);
+        exitButton.setOnClickListener(this);
+    }
+    public void onClick(View v) {
+        if (v.getId() == returnButton.getId()) {
+
+        }
+        if (v.getId() == exitButton.getId()){
+            finish();
+        }
+    }
 }
+
