ddc7be3b51d1f5f0767d4bce55b8dfb54f97641d
sherif ghaffar <sherif-99@hotmail.com>
2016-03-16 15:26:23 -0400

    MainActivy now links to PickCompTransAct.java when you click continue
    Signed-off-by: sherif ghaffar <sherif-99@hotmail.com>

--- app/src/main/java/com/example/cstuser/assignment2/MainActivity.java
+++ app/src/main/java/com/example/cstuser/assignment2/MainActivity.java
@@ -7,19 +7,28 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    Button exitButton;
+    Button continueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainui);
 
-        Toast.makeText(getApplicationContext(), "Welcome", Toast.LENGTH_SHORT).show();
-        // DISPLAY MESSAGE THAT IS DELAYED
+        // Initializing Buttons and listeners for them
+        exitButton = (Button) this.findViewById(R.id.exitButton);
+        continueButton = (Button) this.findViewById(R.id.continueButton);
 
+        exitButton.setOnClickListener(this);
+        continueButton.setOnClickListener(this);
+
+        Toast.makeText(getApplicationContext(), "Welcome", Toast.LENGTH_SHORT).show();
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -32,7 +41,7 @@ public class MainActivity extends Activity {
 
     //Code for returning results through data from Activity 2
     public void onActivityResult(int requestCode, int resultCode, Intent data)   {
-        if ((requestCode == 1) && (resultCode == RESULT_OK))    {
+        if ((requestCode == 1) && (resultCode == RESULT_OK))   { //AFTER INVESTORS ARE CHOSEN AND RETURN TO MAINACTIVITY
             Toast.makeText(this,data.getData().toString(), Toast.LENGTH_SHORT).show();
         }
         else {
@@ -41,5 +50,21 @@ public class MainActivity extends Activity {
         }
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == exitButton.getId()){
+            finish();
+        }
+        else if (v.getId() == continueButton.getId()){
+            Intent i = new Intent("com.example.cstuser.assignment2.PickCompTransAct");
+
+            // TO CREATE METHOD TO CHECK SELECTED INVESTORS
+            Bundle investor = new Bundle();
+            investor.putString("investor1", "tony");
+            investor.putString("investor2", "maria");
+            i.putExtras(investor);
+           startActivityForResult(i, 1);
+        }
+    }
 }
 
--- app/src/main/java/com/example/cstuser/assignment2/PickCompTransAct.java
+++ app/src/main/java/com/example/cstuser/assignment2/PickCompTransAct.java
@@ -1,7 +1,17 @@
 package com.example.cstuser.assignment2;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+
 /**
  * Created by sherif on 2016-03-12.
  */
-public class PickCompTransAct {
-}
+public class PickCompTransAct extends Activity {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.pickcomptransui);
+        }
+    }
+
